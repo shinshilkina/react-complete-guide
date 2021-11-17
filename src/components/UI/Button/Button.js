4c0c58d9a4e1d938aca62f2a7a47ae9c6ec52861
shinshilkina <anastasiashilina98@gmail.com>
2021-11-17 18:26:04 +0700

    80. Using CSS Modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import styles from './Button.module.css'
+
 // import styled from 'styled-components';
 
 // const Button = styled.button`
@@ -28,7 +31,7 @@ import React from 'react';
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
